794180b2a015082637ac9fc9897af910b2ad6e8d
rahul kumar s <rahu143rspt3gailcom>
2021-03-11 20:04:53 +0530

    complete part 1

---
+++ public/index.js
@@ -0,0 +1,70 @@
+$(".colors").hover(function(){
+  // console.log(this.innerText);
+  var color=this.innerText;
+  $("h1").text(`${color}`);
+  switch(color){
+    case "orange":
+    $("h1").css({
+      "color":"#f58c35",
+      "text-decoration":"underline"
+      // "border":`2px solid ${color}`
+    }
+    )
+      ;
+    break;
+
+    case "yellow":
+    $("h1").css(
+      "color","#f5DE35");
+    break;
+
+    case "green":
+    $("h1").css(
+      "color","#35f5a4");
+    break;
+
+    case "sky-blue":
+    $("h1").css(
+      "color","#35eff5");
+    break;
+
+    case "violet":
+    $("h1").css(
+      "color","#a935f5");
+    break;
+
+
+    case "pink":
+    $("h1").css(
+      "color","#f535d2");
+    break;
+
+  }
+
+  setTimeout(function(){
+    $("h1").text(``);
+  },2000);
+
+})
+
+
+$(document).dblclick(function(){
+  fullscreen();
+})
+
+function fullscreen(){
+  if(document.fullscreenElement){
+    document.exitFullscreen();
+  }else {
+document.documentElement.requestFullscreen().catch(function(e){
+  console.log(e);
+});
+  }
+}
+
+
+// $("h1").hover(function(){
+//
+//   // clearInrerval(interval);
+//
+// })
